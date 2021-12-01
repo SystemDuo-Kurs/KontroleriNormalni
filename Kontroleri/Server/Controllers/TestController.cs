2b60409c50773788fb5e43616fa5f6bc9e045f4f
Dragan Pepic <nedimco2006@gmail.com>
2021-12-01 10:28:37 +0100

    Add project files.

---
+++ Kontroleri/Server/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Kontroleri.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Kontroleri.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    { 
+
+        [HttpPost]
+        public void Foo(Osoba o)
+        {
+            System.IO.File.AppendAllText("osobe.txt", $"{o.Ime} {o.Prezime}"
+                + Environment.NewLine);
+        }
+
+        [HttpGet]
+        public List<Osoba> Osobeee()
+        {
+            List<Osoba> ol = new();
+            foreach(string linija in System.IO.File.ReadLines("osobe.txt"))
+            {
+                string[] imeIprezime = linija.Split(' ');
+                ol.Add(new Osoba { Ime = imeIprezime[0], Prezime = imeIprezime[1] });
+            }
+            return ol;
+        }
+    }
+}
